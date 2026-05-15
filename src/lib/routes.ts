9c33bf80db80c2029afbcd3bffe1e5a446682c0c
Alexey Kashko <126765953+StarveRoK@users.noreply.github.com>
2026-05-15 12:08:03 +0300

    Links update

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -23,10 +23,10 @@ export const ROUTES = {
     },
 
     social: {
-        telegram: '#',
-        vk: '#',
-        max: '#',
-        youtube: '#',
-        dzen: '#',
+        telegram: 'https://t.me/+uAjNNy-_DEFiZGEy',
+        vk: 'https://vk.com/reputa ',
+        max: 'https://rutube.ru/channel/73236118',
+        youtube: 'https://youtube.com/@reputamp',
+        dzen: 'https://dzen.ru/reputamp',
     },
 } as const
